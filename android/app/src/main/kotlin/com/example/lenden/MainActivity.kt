aa479d89773b9d045b8c45b951cbedc04ccf521e
Palette-Interactive <srrohan333@gmail.com>
2023-02-10 01:57:42 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/lenden/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lenden
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+    );
+  }
+}
