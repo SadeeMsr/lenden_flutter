278a3941e9ffdf34c2d16042ddb2af30c78e1ca3
5h0aib <shoaibvice@gmail.com>
2023-02-10 02:16:30 +0600

    tera file structure maine add kiya

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text('Lenden'),
       ),
     );
   }
